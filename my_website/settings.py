2f15b07abea634312c5982d476b31b9fbe936418
Richard Trotter <rich1@mymts.net>
2025-11-19 07:46:58 -0600

    collectstatic

--- my_website/settings.py
+++ my_website/settings.py
@@ -15,7 +15,7 @@ load_dotenv()
 SECRET_KEY = 'django-insecure-t$xr^l%oi5c__m3y*)8b0-wqx$dht*z-v$0x4ua^i(41@nctx-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['richone-designs-production.up.railway.app', 'https://richone-designs-production.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://richone-designs-production.up.railway.app']
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
 STATICFILES_DIRS = ['static/']
 
 # Whitenoise static stuff
