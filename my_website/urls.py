134475c0d8f283024db6935219cb71137aacb2fb
Richard Trotter <rich1@mymts.net>
2025-11-21 07:19:32 -0600

    fixed typo

--- my_website/urls.py
+++ my_website/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include re_path
+from django.urls import path, include, re_path
 from . import settings
 from django.views.static import serve
 from django.conf.urls.static import static
