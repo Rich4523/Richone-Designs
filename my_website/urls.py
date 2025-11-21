61de09be99791528f4d167762c1c2b81ae802782
Richard Trotter <rich1@mymts.net>
2025-11-21 07:14:53 -0600

    added media files

--- my_website/urls.py
+++ my_website/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include re_path
 from . import settings
+from django.views.static import serve
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('info.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
