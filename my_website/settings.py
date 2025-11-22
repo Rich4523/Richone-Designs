ad9a6f9c9d397ae5e4c2c005b2f2acf393027f16
Richard Trotter <rich1@mymts.net>
2025-11-22 08:44:54 -0600

    fixed settings

--- my_website/settings.py
+++ my_website/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://rone-designs.ca', 'richone-designs-production.up.railway.app', 'https://richone-designs-production.up.railway.app']
+ALLOWED_HOSTS = ['https://rone-designs.ca', 'rone-designs.ca', 'richone-designs-production.up.railway.app', 'https://richone-designs-production.up.railway.app']
 CSRF_TRUSTED_ORIGINS = ['https://rone-designs.ca', 'https://richone-designs-production.up.railway.app']
 
 
